cdb438de7c6a5c28a0da349ca0fa3348d305d4a5
Chip Garner <chip@garnertotalenergy.com>
2017-03-27 17:53:49 -0700

    Plays the audio file from AVS but uses an infinite loop

--- test_ask.py
+++ test_ask.py
@@ -1,6 +1,7 @@
 """This test will send a pre-generated audio file in the `tests` directory
 to AVS, and saves it as `/tmp/test1.mp3`."""
 from alexa_client import AlexaClient
+import vlc
 import os
 
 TESTS_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -8,12 +9,14 @@ TESTS_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def main():
     alexa = AlexaClient()
-    input = '{}/1.wav'.format(TESTS_PATH)
-    # input = 'C:/Alexa/Alexa-Client/sfweather.wav'
+    input = '{}/sftides.wav'.format(TESTS_PATH)
     save_to = 'out/test_ask.mp3'
     alexa.ask(input, save_to=save_to)
     print("Response saved to {}".format(save_to))
-
+    p = vlc.MediaPlayer(save_to)
+    p.play()
+    while True:
+        pass
 
 if __name__ == '__main__':
     main()
